dd107e78d073c9d43af1ae7e95f325fae057a6c4
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-10-24 12:46:37 +0200

    Updated

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -7,7 +7,7 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
 
 /**
- * swap_ints - Swap two integers in an array.
+ * swap_ints - switch two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
@@ -74,7 +74,7 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
 }
 
 /**
- * bitonic_sort - Sort an array of integers in ascending
+ * bitonic_sort - put an array of integers in ascending
  *                order using the bitonic sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
